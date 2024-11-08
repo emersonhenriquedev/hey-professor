6b552a37702baef157843a0156e76fe5d8d310c2
Emerson Henrique <emerson.hm@outlook.com>
2024-11-08 20:45:29 -0300

    HEY-8: wip

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -1,6 +1,9 @@
 @props([
     'question'
 ])
-<div class="p-3 text-black bg-white rounded shadow shadow-blue-500/50 dark:bg-gray-800/50 dark:text-gray-400">
-    {{ $question->question }}
+<div class="flex items-center justify-between p-3 text-black bg-white rounded shadow shadow-blue-500/50 dark:bg-gray-800/50 dark:text-gray-400">
+    <span class="">{{ $question->question }}</span>
+    <div>
+        <x-icon.thumbs-up class="w-5 h-5 text-yellow-500 hover:text-yellow-300 hover:cursor-pointer"></x-icon.thumbs-up>
+    </div>
 </div>
