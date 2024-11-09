1cd82254a4abe89d8b751540e1273b1caed8297e
Emerson Henrique <emerson.hm@outlook.com>
2024-11-09 16:24:43 -0300

    HEY-8: unlike button

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -36,3 +36,36 @@ it('should not be able to like more than 1 time', function () {
 
     expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
 });
+
+it('should be able to unlike a question', function () {
+    // arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    // act
+    actingAs($user);
+    post(route('question.unlike', $question))
+        ->assertRedirect();
+
+    // assert
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'like'        => 0,
+        'unlike'      => 1,
+        'user_id'     => $user->id,
+    ]);
+
+});
+
+it('should not be able to unlike more than 1 time', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+
+    expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
+});
